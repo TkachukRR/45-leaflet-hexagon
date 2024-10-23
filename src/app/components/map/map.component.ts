43f6033a16915c450d32f2a0f99e723f538e0649
Roman Tkachuk <tkachukrr@gmail.com>
2024-10-23 13:08:05 +0200

    Added map original leaflet map to the map component.

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import 'proj4leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  map!: L.Map;
 
+  ngOnInit() {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([0, 0], 0);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map)
+  }
 }
