d7a466ac69238f21ffe469f1d4f580d7bd77fd23
Roman Tkachuk <tkachukrr@gmail.com>
2024-10-24 15:12:16 +0200

    Implemented ability to load data

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import 'proj4leaflet';
+import { HttpClient } from '@angular/common/http';
+import { DATA_URL } from '../../constants/url-data.constant';
 
 @Component({
   selector: 'app-map',
   standalone: true,
   imports: [],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
   map!: L.Map;
+  private data = {};
+  private readonly DATA_URL = DATA_URL;
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.initMap();
+    this.loadData();
   }
 
   private initMap(): void {
@@ -22,5 +28,13 @@ export class MapComponent implements OnInit {
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map)
+  }
+
+  private loadData(): void {
+    this.http.get<any>(this.DATA_URL).subscribe((response) => {
+      this.data = response;
+    }, (error) => {
+      console.error('Error loading data:', error);
+    });
   }
 }
---
+++ src/app/constants/url-data.constant.ts
@@ -0,0 +1,1 @@
+export const DATA_URL = 'https://raw.githubusercontent.com/gis-point/angular-hexagon-test/main/data.json';
