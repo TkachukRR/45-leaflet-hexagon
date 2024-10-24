7861587def74f14ae34ec4a731d6b77b3e6abf4a
Roman Tkachuk <tkachukrr@gmail.com>
2024-10-24 15:27:08 +0200

    Added data convert coordinates method Installed dependencies

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
 import { DATA_URL } from '../../constants/url-data.constant';
+import proj4 from 'proj4';
 
 @Component({
   selector: 'app-map',
@@ -12,7 +13,7 @@ import { DATA_URL } from '../../constants/url-data.constant';
 })
 export class MapComponent implements OnInit {
   map!: L.Map;
-  private data = {};
+  private convertedData = {};
   private readonly DATA_URL = DATA_URL;
 
   constructor(private http: HttpClient) {}
@@ -32,9 +33,30 @@ export class MapComponent implements OnInit {
 
   private loadData(): void {
     this.http.get<any>(this.DATA_URL).subscribe((response) => {
-      this.data = response;
+      this.convertedData = this.convertCoordinates(response.features);
     }, (error) => {
       console.error('Error loading data:', error);
+    });
+  }
+
+  private convertCoordinates(features: any[]): any[] {
+    const EPSG3857 = 'EPSG:3857';
+    const EPSG4326 = 'EPSG:4326';
+
+    return features.map(feature => {
+      const convertedCoordinates = feature.geometry.coordinates.map((polygon: any[]) =>
+        polygon.map(ring =>
+          ring.map((point: any) => proj4(EPSG3857, EPSG4326, point))
+        )
+      );
+
+      return {
+        ...feature,
+        geometry: {
+          ...feature.geometry,
+          coordinates: convertedCoordinates
+        }
+      };
     });
   }
 }
